8f5bb1d318fffdb5ad3e33de393284655227e22c
Aaron Welsh <aaronwelsh505@gmail.com>
2018-11-14 20:38:09 +0000

    Conditional rendering added for Chat component and a username form. We will then store the inputed username (or generated name) as a cookie. Bunch of methods added to set, save and check username.

--- src/public/views/Home.vue
+++ src/public/views/Home.vue
@@ -1,7 +1,19 @@
 <template>
     <div class="pure-g">
         <div class="pure-u-1">
-            <chat></chat>
+
+            <form v-if="!user.set" class="pure-form" v-on:submit.prevent="setUsername" id="username-form">
+                <strong>Pick a name</strong>            
+                <fieldset class="pure-group">
+                    <input type="text" class="pure-input-1-3" id="username" v-model="user.name" placeholder="Your username" />
+                </fieldset>
+            </form>
+            <button v-on:click="randomUsername" v-if="!user.set" type="submit" class="pure-button pure-input-1-5 pure-button-primary">Or generate a random username!</button>
+
+
+            <strong v-if="user.set">Hi, {{user.name}}</strong>
+            <chat v-if="user.set" :user-data="user"></chat>
+
         </div>
     </div>
 </template>
@@ -10,14 +22,61 @@
     import Chat from './components/Chat.vue';
 
     export default {
+
         components: {
             'chat' : Chat
         },
-        data () {
-            return { 
-               
-            }
+        mounted: function() {
+
+            this.checkUsername(this.user.name);
+
         },
+        data () {
+
+            return { 
+               user: {
+                   name: '',
+                   set: false
+               }
+            }
+
+        },
+        methods: {
+
+            setUsername: function(event) {
+
+                event.preventDefault();
+                this.saveUsername(this.user.name);
+
+            },
+            saveUsername: function(username) {
+
+                const date = new Date;
+                date.setDate(date.getDate() + 7);
+                document.cookie = "username="+username+"; expires=" + date + ";";
+                this.user.set = true;
+                this.user.name = username;
+
+            },
+            checkUsername: function(username) {
+
+                const usernameCookie = document.cookie.replace(/(?:(?:^|.*;\s*)username\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+                if(usernameCookie) {
+                    this.user.set = true;
+                    this.user.name = usernameCookie;
+                }
+
+            },
+            randomUsername: function() {
+
+                // Random username consisting of 'anon' and 5 random numbers.
+                const random = Math.floor(Math.random()*90000) + 10000;
+                const username = `anon${random}`;
+                this.saveUsername(username);
+
+
+            }
+        }
 
     }
     
